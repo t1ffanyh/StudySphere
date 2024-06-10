ef0c4a9a73236e16432a917ad8f5eb18c380cad3
tifanyh <341118578@gapps.yrdsb.ca>
2024-06-10 19:32:05 -0400

    Initial Commit

---
+++ StudySphere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudySphere
+//
+//  Created by Tiffany Ho on 2024-06-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
